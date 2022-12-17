d549b7a795a2b5576993b340a0f5ebf4be14c743
Daniele Cortesi <dani.c93@hotmail.it>
2022-12-17 23:39:28 +0100

    Tweak action probabilities

--- src/ai/ifThenElse/handActions.ts
+++ src/ai/ifThenElse/handActions.ts
@@ -25,30 +25,30 @@ export function strongHandAction(state: State): Action {
 export function riskyHandAction(state: State): Action {
     return probabilisticAction("risky", state, uniformFill({
         checkFoldProbability: state.toCall > 0 ? 0.3 : 0.5,
-        callProbability: 0.3,
+        callProbability: state.toCall > 0 ? 0.3 : 0,
     }));
 }
 
 export function weakHandAction(state: State): Action {
     return probabilisticAction("weak", state, uniformFill({
         checkFoldProbability: state.toCall > 0 ? 0.5 : 0.7,
-        callProbability: 0.2,
+        callProbability: state.toCall > 0 ? 0.2 : 0,
     }));
 }
 
 export function bluffHandAction(state: State): Action {
     if (state.toCall > 0) {
         return probabilisticAction("bluff-c", state, zeroFill({
-            checkFoldProbability: 0.9,
-            potRaiseProbability: 0.1 / 2,
-            allInProbability: 0.1 / 2,
+            checkFoldProbability: 0.95,
+            potRaiseProbability: 0.05 / 2,
+            allInProbability: 0.05 / 2,
         }));
     }
     else {
         return probabilisticAction("bluff", state, zeroFill({
-            checkFoldProbability: 0.8,
-            potRaiseProbability: 0.2 / 2,
-            allInProbability: 0.2 / 2,
+            checkFoldProbability: 0.9,
+            potRaiseProbability: 0.1 / 2,
+            allInProbability: 0.1 / 2,
         }));
     }
 }

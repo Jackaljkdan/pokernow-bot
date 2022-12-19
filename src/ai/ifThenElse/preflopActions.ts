03bad75f0919c214a4eecf70cd11ac4ab1045c53
Daniele Cortesi <dani.c93@hotmail.it>
2022-12-19 23:47:31 +0100

    Improve preflop action probabilities

--- src/ai/ifThenElse/preflopActions.ts
+++ src/ai/ifThenElse/preflopActions.ts
@@ -1,8 +1,8 @@
 import { AceCode, JackCode, QueenCode } from "../../cards";
 import { getHighestCard, getLowestCard } from "../aiUtils";
 import { lerp } from "../lerp";
-import { probabilisticAction, uniformFill } from "../probabilisticAction";
-import { bestHandAction, bluffHandAction, riskyHandAction, strongHandAction, weakHandAction } from "./handActions";
+import { checkCallBased, postfixNameToCall, probabilisticAction, toCallDependent, uniformFill } from "../probabilisticAction";
+import { bestHandAction } from "./handActions";
 
 
 export function preflopAction(state: State): Action {
@@ -21,13 +21,15 @@ function pairPreflopAction(state: State): Action {
         return bestHandAction(state);
     
     if (valueCode >= 10)
-        return strongHandAction(state);
+        return highPairAction(state);
 
     const ninesCheckOrFoldProbability = 0.05;
     const twosCheckOrFoldProbability = 0.8;
 
-    return probabilisticAction("lowpair", state, uniformFill({
+    return probabilisticAction("pre-lowpair", state, uniformFill({
         checkFoldProbability: lerp(twosCheckOrFoldProbability, ninesCheckOrFoldProbability, (valueCode - 2) / 9),
+        halfPotRaiseProbability: 0,
+        allInProbability: 0,
     }));
 }
 
@@ -36,19 +38,119 @@ function nonPairPreflopAction(state: State): Action {
     const lowestCard = getLowestCard(state.hand)!;
 
     if (lowestCard.value.code >= QueenCode)
-        return strongHandAction(state);
+        return premiumNonPairAction(state);
 
     if (lowestCard.value.code >= 10)
-        return riskyHandAction(state);
+        return semiHighNonPairAction(state);
     
     const delta = highestCard.value.code - lowestCard.value.code;
 
     // TODO: differenziare ulteriormente, le mani con l'asso non sono così bluffose per dire
     if (delta >= 5)
-        return bluffHandAction(state);
+        return trashAction(state);
 
     if (highestCard.value.code >= JackCode)
-        return weakHandAction(state);
+        return faceAction(state);
 
-    return bluffHandAction(state);
+    return trashAction(state);
+}
+
+function highPairAction(state: State): Action {
+    return probabilisticAction(postfixNameToCall("pre-highpair", state), state, toCallDependent(state, {
+        zero: {
+            checkFoldProbability: 0,
+            minRaiseProbability: 0.5,
+            halfPotRaiseProbability: 0,
+            potRaiseProbability : 0.5,
+            allInProbability: 0,
+        },
+        nonZero: checkCallBased({
+            checkFoldProbability: 0,
+            callProbability: state.toCall >= 3 * state.bigBlind ? 0.5 : 0,
+            remainingMinRaiseShare: 0.5,
+            remainingHalfPotRaiseShare: 0,
+            remainingPotRaiseShare: 0.5,
+            remainingAllInShare: 0,
+        }),
+    }));
+}
+
+function premiumNonPairAction(state: State): Action {
+    return probabilisticAction(postfixNameToCall("pre-high-nonpair", state), state, toCallDependent(state, {
+        zero: {
+            checkFoldProbability: 0,
+            minRaiseProbability: 0.6,
+            halfPotRaiseProbability: 0,
+            potRaiseProbability : 0.4,
+            allInProbability: 0,
+        },
+        nonZero: checkCallBased({
+            checkFoldProbability: 0,
+            callProbability: state.toCall >= 3 * state.bigBlind ? 0.5 : 0,
+            remainingMinRaiseShare: 0.7,
+            remainingHalfPotRaiseShare: 0,
+            remainingPotRaiseShare: 0.3,
+            remainingAllInShare: 0,
+        }),
+    }));
+}
+
+function semiHighNonPairAction(state: State): Action {
+    return probabilisticAction(postfixNameToCall("pre-semihigh-nonpair", state), state, toCallDependent(state, {
+        zero: {
+            checkFoldProbability: 0,
+            minRaiseProbability: 0.8,
+            halfPotRaiseProbability: 0,
+            potRaiseProbability : 0.2,
+            allInProbability: 0,
+        },
+        nonZero: checkCallBased({
+            checkFoldProbability: 0,
+            callProbability: state.toCall >= 3 * state.bigBlind ? 0.7 : 0,
+            remainingMinRaiseShare: 0.8,
+            remainingHalfPotRaiseShare: 0,
+            remainingPotRaiseShare: 0.2,
+            remainingAllInShare: 0,
+        }),
+    }));
+}
+
+function trashAction(state: State): Action {
+    return probabilisticAction(postfixNameToCall("pre-trash", state), state, toCallDependent(state, {
+        zero: {
+            checkFoldProbability: 0.9,
+            minRaiseProbability: 0,
+            halfPotRaiseProbability: 0,
+            potRaiseProbability : state.toCall >= 3 * state.bigBlind ? 0.08 : 0,
+            allInProbability: 0.02,
+        },
+        nonZero: checkCallBased({
+            checkFoldProbability: 0.95,
+            callProbability: 0,
+            remainingMinRaiseShare: 0,
+            remainingHalfPotRaiseShare: 0,
+            remainingPotRaiseShare: state.toCall >= 3 * state.bigBlind ? 0.03 : 0,
+            remainingAllInShare: 0.02,
+        }),
+    }));
+}
+
+function faceAction(state: State): Action {
+    return probabilisticAction(postfixNameToCall("pre-face", state), state, toCallDependent(state, {
+        zero: {
+            checkFoldProbability: 0.7,
+            minRaiseProbability: 0.2,
+            halfPotRaiseProbability: 0.1,
+            potRaiseProbability : 0,
+            allInProbability: 0,
+        },
+        nonZero: checkCallBased({
+            checkFoldProbability: 0.7,
+            callProbability: state.toCall <= 2 * state.bigBlind ? 0.2 : 0,
+            remainingMinRaiseShare: 0,
+            remainingHalfPotRaiseShare: 0,
+            remainingPotRaiseShare: 0,
+            remainingAllInShare: 0,
+        }),
+    }));
 }

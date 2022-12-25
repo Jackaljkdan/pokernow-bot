dee236e3b3747b7795c0cc45d5700c36d60978cd
Daniele Cortesi <dani.c93@hotmail.it>
2022-12-25 21:55:50 +0100

    Improve readability

--- src/ai/ifThenElse/straightAction.ts
+++ src/ai/ifThenElse/straightAction.ts
@@ -7,8 +7,9 @@ export function straightAction(state: State): Action {
 
     const myStraight = findBestStraight(state.handPlusBoard)!;
     const bestGapStraight = findBestGapStraight(state.board);
+    const isBetterStraightPossible = bestGapStraight !== null && myStraight.min.value.code < bestGapStraight.firstPiece.min.value.code;
 
-    if (bestGapStraight !== null && myStraight.min.value.code < bestGapStraight.firstPiece.min.value.code)
+    if (isBetterStraightPossible)
         return riskyHandAction(state);
     
     return strongHandAction(state);
